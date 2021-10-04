e1b137aa35e37bcc0ada08ce596409d469378e6c
tei <75574873+TeiV2@users.noreply.github.com>
2021-10-04 01:42:26 -0300

    JS

---
+++ app.js
@@ -0,0 +1,21 @@
+window.onload = main;
+// console.log("hola mundo");
+function main() {
+    let btnCalcular = document.getElementById("btn-calcular");
+    btnCalcular.addEventListener('click', calcularTabla);
+}
+function obtenerValorDeTextBox() {
+    let inputValueRaw = document.getElementById("num-tabla");
+    if (+inputValueRaw.value === 0) {
+        return 1;
+    }
+    return +`${inputValueRaw.value}`;
+}
+function calcularTabla() {
+    let multiply = obtenerValorDeTextBox();
+    for (let i = 1; i <= 12; i++) {
+        document.getElementById(`th${i}`).innerText = `${multiply} * ${i}`;
+        document.getElementById(`td${i}`).innerText = `${multiply * i}`;
+    }
+}
+//# sourceMappingURL=app.js.map
